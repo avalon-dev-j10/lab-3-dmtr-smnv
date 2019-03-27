89dd3bee9d0d6d445609dea62894a26065b0bb11
Semenov_D <8933626@mail.ru>
2019-03-27 14:53:24 +0300

    Лабораторная работа #3

--- src/ru/avalon/java/dev/j10/labs/initialization/RandomInitializer.java
+++ src/ru/avalon/java/dev/j10/labs/initialization/RandomInitializer.java
@@ -1,29 +1,44 @@
 package ru.avalon.java.dev.j10.labs.initialization;
 
 import ru.avalon.java.dev.j10.labs.Initializer;
+import java.util.Random;
 
 /**
- * Инициализатор, выполняющий инициализацию массива
- * значениями последовательности случайных чисел.
+ * Инициализатор, выполняющий инициализацию массива значениями
+ * последовательности случайных чисел.
  *
- * <p>Экземпляры класса можно использовать многократно для
- * инициализации любого числа массивов.
+ * <p>
+ * Экземпляры класса можно использовать многократно для инициализации любого
+ * числа массивов.
  *
- * <p>Диапазон, в пределах которого будут получены случайные
- * значения, передаётся в конструктор класса при
- * инициализации.
+ * <p>
+ * Диапазон, в пределах которого будут получены случайные значения, передаётся в
+ * конструктор класса при инициализации.
  */
 public class RandomInitializer implements Initializer {
 
+    private final int minLimit;
+    private final int maxLimit;
+
+    public RandomInitializer(int minLimit, int maxLimit) {
+        this.minLimit = minLimit;
+        this.maxLimit = maxLimit;
+    }
+
+    private final Random random = new Random();
+
     /**
-     * Выполняет инициализацию массива, значениями
-     * последовательности случайных чисел.
+     * Выполняет инициализацию массива, значениями последовательности случайных
+     * чисел.
      *
-     * @param array массив, подлежащий инициализации
+     * @param array - инициализируемый массив
      */
+    @Override
     public void initialize(int[] array) {
-        /*
-         * TODO(Студент): Реализовать метод initialize класса RandomInitializer
-         */
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(maxLimit - minLimit) + minLimit;
+        }
+
     }
 }
